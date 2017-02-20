d9993830a4438e39feafddd70dd62671ed5ea727
Thomaswu1106 <thomas1106@gmail.com>
2017-02-20 12:06:33 +0800

    add reserved rg not deallcate
    Exclude the reserved rg

--- rg_vm_deallocate.py
+++ rg_vm_deallocate.py
@@ -25,7 +25,7 @@ credentials = ServicePrincipalCredentials(
 client = ResourceManagementClient(credentials, subscription_id)
 compute_client = ComputeManagementClient(credentials, subscription_id)
 rg_share_name = 'epas-rg-shared'
-rg_name = 'epas-devqa2-1'
+reserved_rg = 'epas-rg-devqa2-1'
 
 class consumer(threading.Thread):  
     def __init__(self,que):  
@@ -36,20 +36,22 @@ class consumer(threading.Thread):
         while True:  
             if self.queue.empty():  
                 break  
-            vm_name = self.queue.get()
-            st = int(time.time())
-            async_vm_deallocate = compute_client.virtual_machines.deallocate( rg_name , vm_name)
-            async_vm_deallocate.wait()
-            et = int(time.time()) - st # delete execution time
-            print("Deallocate VM %s in RG %s spend %d seconds\t" % (format(vm_name), rg_name, et))
+            rg_name = self.queue.get()
+            for vm_name in compute_client.virtual_machines.list(rg_name):
+                st = int(time.time())
+                async_vm_deallocate = compute_client.virtual_machines.deallocate( rg_name , vm_name)
+                async_vm_deallocate.wait()
+                et = int(time.time()) - st # delete execution time
+                print("Deallocate VM %s in RG %s spend %d seconds\t" % (format(vm_name), rg_name, et))
             self.queue.task_done()  
         return  
 
 def run_queue():
     que = Queue.Queue()
 
-    for vm_list in compute_client.virtual_machines.list(rg_name):
-        que.put(format(vm_list.name))
+    for rg_list in client.resource_groups.list():
+        if rg_list.name not in rg_share_name or reserved_rg:
+            que.put(format(rg_list.name))
 
     consumers = [consumer(que) for i in range(0,que.qsize())]
 
